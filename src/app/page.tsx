c574b9f741125e88d24af4c666f9a69eac46ec3d
junayed-code <junayed.code@gmail.com>
2024-01-25 11:27:00 +0600

    Update root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <main></main>;
+import Countries from "@/components/Countries";
+import CountryQuery from "@/components/CountryQuery";
+import { Suspense } from "react";
+
+type Props = {
+  params: any;
+  searchParams: { page?: string; region?: string; search?: string };
+};
+
+export default async function Home({ searchParams }: Props) {
+  return (
+    <>
+      <Suspense fallback={<></>}>
+        <CountryQuery />
+      </Suspense>
+      <Suspense
+        fallback={
+          <div className="text-3xl font-bold text-center">Loading...</div>
+        }
+      >
+        <Countries searchParams={searchParams} />
+      </Suspense>
+    </>
+  );
 }
