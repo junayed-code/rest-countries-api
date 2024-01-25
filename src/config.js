e52333a394529a6024dd3dab53fda76942205b31
junayed-code <junayed.code@gmail.com>
2024-01-25 11:20:08 +0600

    Add config.ts file

---
+++ src/config.js
@@ -0,0 +1,15 @@
+export default {
+  BASE_URL: "http://localhost:3000",
+  FILTER_REGIONS: ["Africa", "Europe", "Americas", "Asia", "Oceania"],
+  PREFETCH_COUNTRIES: [
+    "Bangladesh",
+    "Pakistan",
+    "Saudi Arabia",
+    "India",
+    "Afghanistan",
+    "France",
+    "Germany",
+    "United Kingdom of Great Britain and Northern Ireland",
+    "United States of America",
+  ],
+};
