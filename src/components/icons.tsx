a3189b18ec0ed831700d6e07903d208557757edc
junayed-code <junayed.code@gmail.com>
2024-01-25 11:00:14 +0600

    Add icons component

---
+++ src/components/icons.tsx
@@ -0,0 +1,77 @@
+type IconProps = {
+  fill?: string;
+  className?: string;
+};
+
+export const MoonIcon = ({ fill = "none", className }: IconProps) => (
+  <svg
+    className={className}
+    stroke="currentColor"
+    fill="currentColor"
+    strokeWidth="0"
+    viewBox="0 0 512 512"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill={fill}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="32"
+      d="M160 136c0-30.62 4.51-61.61 16-88C99.57 81.27 48 159.32 48 248c0 119.29 96.71 216 216 216 88.68 0 166.73-51.57 200-128-26.39 11.49-57.38 16-88 16-119.29 0-216-96.71-216-216z"
+    ></path>
+  </svg>
+);
+
+export const SearchIcon = ({ fill = "currentColor", className }: IconProps) => (
+  <svg
+    className={className}
+    stroke="currentColor"
+    fill={fill}
+    strokeWidth="0"
+    viewBox="0 0 512 512"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M443.5 420.2L336.7 312.4c20.9-26.2 33.5-59.4 33.5-95.5 0-84.5-68.5-153-153.1-153S64 132.5 64 217s68.5 153 153.1 153c36.6 0 70.1-12.8 96.5-34.2l106.1 107.1c3.2 3.4 7.6 5.1 11.9 5.1 4.1 0 8.2-1.5 11.3-4.5 6.6-6.3 6.8-16.7.6-23.3zm-226.4-83.1c-32.1 0-62.3-12.5-85-35.2-22.7-22.7-35.2-52.9-35.2-84.9 0-32.1 12.5-62.3 35.2-84.9 22.7-22.7 52.9-35.2 85-35.2s62.3 12.5 85 35.2c22.7 22.7 35.2 52.9 35.2 84.9 0 32.1-12.5 62.3-35.2 84.9-22.7 22.7-52.9 35.2-85 35.2z"></path>
+  </svg>
+);
+
+export const ArrowLeftIcon = ({
+  fill = "currentColor",
+  className,
+}: IconProps) => (
+  <svg
+    className={className}
+    stroke="currentColor"
+    fill={fill}
+    strokeWidth="0"
+    viewBox="0 0 16 16"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+    ></path>
+  </svg>
+);
+
+export const ArrowDownIcon = ({ fill = "none", className }: IconProps) => (
+  <svg
+    className={className}
+    fill="currentColor"
+    stroke="currentColor"
+    strokeWidth="0"
+    viewBox="0 0 24 24"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path fill={fill} d="M0 0h24v24H0V0z"></path>
+    <path d="M7.41 8.59 12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"></path>
+  </svg>
+);
