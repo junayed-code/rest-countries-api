bd7c397f915f03dd30da9878972d8ebc70fef03e
junayed-code <junayed.code@gmail.com>
2024-01-25 11:07:19 +0600

    Create ThemeProvider component to manage the app theme

---
+++ src/providers/ThemeProvider.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React, { useEffect, useState, createContext } from "react";
+
+type Theme = "dark" | "light";
+type ThemeProviderValues = {
+  theme: Theme;
+  toggleTheme: (theme?: Theme) => void;
+};
+
+export const ThemeContext = createContext<ThemeProviderValues>();
+
+function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [currentTheme, setCurrentTheme] = useState<Theme>("light");
+
+  const toggleTheme = (theme?: any) => {
+    setCurrentTheme(prev => {
+      const THEME = theme || (prev === "dark" ? "light" : "dark");
+      document.documentElement.classList.remove("dark", "light");
+      document.documentElement.classList.add(THEME);
+      sessionStorage.setItem("__theme", THEME);
+      return THEME;
+    });
+  };
+
+  useEffect(() => {
+    const mql = window.matchMedia("(prefers-color-scheme: dark)");
+    const __theme = sessionStorage.getItem("__theme");
+    toggleTheme(__theme || (mql.matches ? "dark" : "light"));
+    const handleThemeChange = (e: MediaQueryListEvent) => {
+      toggleTheme(e.matches ? "dark" : "light");
+    };
+
+    mql.addEventListener("change", handleThemeChange);
+
+    return () => {
+      mql.removeEventListener("change", handleThemeChange);
+    };
+  }, []);
+
+  return (
+    <ThemeContext.Provider value={{ theme: currentTheme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export default ThemeProvider;
