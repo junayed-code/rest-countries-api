640a5791be55b40992b59cac4a2817aaeb517f17
junayed-code <junayed.code@gmail.com>
2024-01-25 10:54:41 +0600

    Update tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,16 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
+    container: {
+      center: true,
+      padding: "1.25rem",
+      screens: {
+        lg: "1024px",
+        xl: "1220px",
+      },
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-nunito-sans)"],
