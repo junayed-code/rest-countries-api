d26db1090d606cd1e6d1699b5ee46cba6eb6bbba
junayed-code <junayed.code@gmail.com>
2024-01-27 23:36:11 +0600

    Update country details page

--- src/app/country/[name]/page.tsx
+++ src/app/country/[name]/page.tsx
@@ -1,5 +1,6 @@
 import BackButton from "@/components/BackButton";
 import config from "@/config";
+import getCountryByName from "@/utils/getCountryByName";
 import slugify from "slugify";
 
 type Props = {
@@ -13,110 +14,107 @@ export function generateStaticParams() {
 }
 
 async function CountryDetailsPage({ params }: Props) {
-  try {
-    const res = await fetch(
-      `${config.BASE_URL}/api/countries/name/${params.name}`
-    );
-    const country = await res.json();
+  const country = await getCountryByName(params.name);
+  const countryName = country.name?.common;
+  const nativeName = Object.values<{ common: string }>(
+    country.name?.nativeName
+  )?.[0]?.common;
+  const currencies = Object.values<{ name: string }>(country.currencies)?.[0]
+    .name;
+  const languages = Object.values(country.languages);
 
-    return (
-      <>
-        <div className="my-12">
-          <BackButton />
-        </div>
+  return (
+    <>
+      <div className="my-12">
+        <BackButton />
+      </div>
 
-        <div className="flex flex-col max-w-2xl lg:max-w-full mx-auto lg:flex-row gap-x-20 gap-y-10 mt-4 mb-12">
-          <figure className="basis-full">
-            <img
-              className="w-full object-cover aspect-[2.5_/_1.5] shadow-[0_0_15px_-4px] shadow-black/30 rounded-md"
-              src={country.flags?.png}
-              alt={`${country.name}'s flag image`}
-            />
-          </figure>
+      <div className="flex flex-col max-w-2xl lg:max-w-full mx-auto lg:flex-row gap-x-20 gap-y-10 mt-4 mb-12">
+        <figure className="basis-full">
+          <img
+            className="w-full object-cover aspect-[2.5_/_1.5] shadow-[0_0_15px_-4px] shadow-black/30 rounded-md"
+            src={country.flags?.png}
+            alt={`Flag of ${countryName}`}
+          />
+        </figure>
 
-          <div className="basis-full">
-            <h2 className="text-[28px] font-bold">{country.name}</h2>
-            <div className="mt-6 flex flex-col sm:flex-row gap-y-6 gap-x-16 items-start leading-8">
-              <div>
-                <p className="font-semibold">
-                  Native Name:{" "}
-                  <span className="font-normal text-gray-600 dark:text-slate-300">
-                    {country.nativeName}
-                  </span>
-                </p>
-                <p className="font-semibold">
-                  Population:{" "}
-                  <span className="font-normal text-gray-600 dark:text-slate-300">
-                    {country.population}
-                  </span>
-                </p>
-                <p className="font-semibold">
-                  Region:{" "}
-                  <span className="font-normal text-gray-600 dark:text-slate-300">
-                    {country.region}
-                  </span>
-                </p>
-                <p className="font-semibold">
-                  Sub Region:{" "}
-                  <span className="font-normal text-gray-600 dark:text-slate-300">
-                    {country.subregion}
-                  </span>
-                </p>
-                <p className="font-semibold">
-                  Capital:{" "}
-                  <span className="font-normal text-gray-600 dark:text-slate-300">
-                    {country.capital}
-                  </span>
-                </p>
-              </div>
-
-              <div>
-                <p className="font-semibold">
-                  Top Level Domain:{" "}
-                  <span className="font-normal text-gray-600 dark:text-slate-300">
-                    {country.topLevelDomain}
-                  </span>
-                </p>
-                <p className="font-semibold">
-                  Currencies:{" "}
-                  <span className="font-normal text-gray-600 dark:text-slate-300">
-                    {country.currencies?.[0]?.name}
-                  </span>
-                </p>
-                <p className="font-semibold">
-                  Languages:{" "}
-                  <span className="font-normal text-gray-600 dark:text-slate-300">
-                    {country.languages
-                      ?.map((language: any) => language.name)
-                      ?.slice(0, 3)
-                      ?.join(", ")}
-                  </span>
-                </p>
-              </div>
-            </div>
-
-            {country.borders?.length > 0 && (
-              <p className="font-semibold mt-10 flex flex-wrap items-center gap-3">
-                <span>Border Countries:</span>
-                <span className="font-normal flex flex-wrap gap-2 ml-1">
-                  {country.borders.map((border: string) => (
-                    <span
-                      key={border}
-                      className="inline-block rounded-sm px-4 py-1 shadow-[0_0_10px_-2px] shadow-black/30 bg-white dark:bg-slate-700 dark:text-slate-50"
-                    >
-                      {border}
-                    </span>
-                  ))}
+        <div className="basis-full">
+          <h2 className="text-[28px] font-bold">{countryName}</h2>
+          <div className="mt-6 flex flex-col sm:flex-row gap-y-6 gap-x-16 items-start leading-8">
+            <div>
+              <p className="font-semibold">
+                Native Name:{" "}
+                <span className="font-normal text-gray-600 dark:text-slate-300">
+                  {nativeName}
                 </span>
               </p>
-            )}
+              <p className="font-semibold">
+                Population:{" "}
+                <span className="font-normal text-gray-600 dark:text-slate-300">
+                  {country.population}
+                </span>
+              </p>
+              <p className="font-semibold">
+                Region:{" "}
+                <span className="font-normal text-gray-600 dark:text-slate-300">
+                  {country.region}
+                </span>
+              </p>
+              <p className="font-semibold">
+                Sub Region:{" "}
+                <span className="font-normal text-gray-600 dark:text-slate-300">
+                  {country.subregion}
+                </span>
+              </p>
+              <p className="font-semibold">
+                Capital:{" "}
+                <span className="font-normal text-gray-600 dark:text-slate-300">
+                  {country.capital?.[0]}
+                </span>
+              </p>
+            </div>
+
+            <div>
+              <p className="font-semibold">
+                Top Level Domain:{" "}
+                <span className="font-normal text-gray-600 dark:text-slate-300">
+                  {country.tld?.[0]}
+                </span>
+              </p>
+              <p className="font-semibold">
+                Currencies:{" "}
+                <span className="font-normal text-gray-600 dark:text-slate-300">
+                  {currencies}
+                </span>
+              </p>
+              <p className="font-semibold">
+                Languages:{" "}
+                <span className="font-normal text-gray-600 dark:text-slate-300">
+                  {languages?.slice(0, 3)?.join(", ")}
+                </span>
+              </p>
+            </div>
           </div>
+
+          {country.borders?.length > 0 && (
+            <p className="font-semibold mt-10 flex flex-wrap items-center gap-3">
+              <span>Border Countries:</span>
+              <span className="font-normal flex flex-wrap gap-2 ml-1">
+                {country.borders.map((border: string) => (
+                  <span
+                    key={border}
+                    className="inline-block rounded-sm px-4 py-1 shadow-[0_0_10px_-2px] shadow-black/30 bg-white dark:bg-slate-700 dark:text-slate-50"
+                  >
+                    {border}
+                  </span>
+                ))}
+              </span>
+            </p>
+          )}
         </div>
-      </>
-    );
-  } catch {
-    return null;
-  }
+      </div>
+    </>
+  );
 }
 
 export default CountryDetailsPage;
